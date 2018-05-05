37359084aa36b4909906638e2f460d70f8b997b6
wr0ngc0degen <wr0ngc0degen@gmail.com>
2018-05-05 19:51:59 +0200

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -22,11 +23,28 @@ app.post('/todos', (req, res) => {
 });
 
 app.get('/todos', (req, res) => {
-   Todo.find().then((todos) => {
-       res.send({todos});
-   }, (e) => {
-       res.status(400).send(e);
-   });
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            res.status(404).send();
+        }
+        res.status(200).send(todo);
+    }).catch((e) => {
+        res.status(400).send();
+    });
+
+
 });
 
 app.listen(3000, () => {
