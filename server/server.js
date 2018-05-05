4fc116e0c620a04a847ecaf0c1e6cb07d921bba1
wr0ngc0degen <wr0ngc0degen@gmail.com>
2018-05-05 18:36:08 +0200

    correct a typo

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.get('/todos', (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log('St˚arted on port 3000');
+    console.log('Started on port 3000');
 });
 
 module.exports = {app};
