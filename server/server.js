9c0330639f6a3877b95809f545cf27f213994eb9
wr0ngc0degen <wr0ngc0degen@gmail.com>
2018-05-05 15:07:00 +0200

    correction to the imports

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/Todo');
-var {User} = require('./models/User');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 var app = express();
 
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -2,7 +2,7 @@ const expect = require('expect');
 const request = require('supertest');
 
 const {app} = require('./../server');
-const {Todo} = require('./../models/Todo');
+const {Todo} = require('./../models/todo');
 
 beforeEach((done) => {
    Todo.remove({}).then(() => done());
