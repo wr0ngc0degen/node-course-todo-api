8f8b17dd6be1c0b49856913f46556fc69a9bae83
wr0ngc0degen <wr0ngc0degen@gmail.com>
2018-05-21 16:52:23 +0200

    make DELETE return the array

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ app.get('/todos/:id', (req, res) => {
         if (!todo) {
             res.status(404).send();
         }
-        res.status(200).send(todo);
+        res.send({todo});
     }).catch((e) => {
         res.status(400).send();
     });
@@ -66,7 +66,7 @@ app.delete('/todos/:id', (req, res) => {
         if (!todo) {
             res.status(404).send();
         }
-        res.status(200).send(todo);
+        res.status(200).send({todo});
     }).catch((e) => {
         res.status(400).send();
     });
