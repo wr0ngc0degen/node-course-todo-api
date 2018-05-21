1d6003b678f505fae70ebead32d2b53496aa8f4d
wr0ngc0degen <wr0ngc0degen@gmail.com>
2018-05-21 18:00:25 +0200

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -78,7 +78,7 @@ describe('GET /todos/:id', () => {
             .get(`/todos/${todos[0]._id.toHexString()}`)
             .expect(200)
             .expect((res) => {
-                expect(res.body.text).toBe(todos[0].text)
+                expect(res.body.todo.text).toBe(todos[0].text)
             })
             .end(done);
     });
@@ -96,4 +96,41 @@ describe('GET /todos/:id', () => {
             .expect(404)
             .end(done);
     })
+});
+
+describe('DELETE /todos/:id', () => {
+   it('should remove a todo', (done) => {
+       var hexId = todos[1]._id.toHexString();
+
+       request(app)
+           .delete(`/todos/${hexId}`)
+           .expect(200)
+           .expect((res) => {
+               expect(res.body.todo._id).toBe(hexId);
+           })
+           .end((err, res) => {
+               if(err) {
+                   return done(err);
+               }
+
+               Todo.findById(hexId).then((todo) => {
+                   expect(todo).toNotExist();
+                   done();
+               }).catch((e) => done(e));
+           });
+   });
+
+   it('should return 404 if todo not found', (done) => {
+       request(app)
+           .delete(`/todos/${new ObjectID().toHexString()}`)
+           .expect(404)
+           .end(done);
+   });
+
+   it('should return 404 if object id is invalid', (done) => {
+       request(app)
+           .delete(`/todos/${todos[0]._id.toHexString()}1`)
+           .expect(404)
+           .end(done);
+   });
 });
